a0f87020abab57443b98e765400c0ba8d8b905ba
friendlyPrime <friendlyPrime@gmail.com>
2017-06-11 00:53:51 +0200

    Present till victor

---
+++ ShitChatter/src/main/java/com/hardamyggan/shitchatter/SimpleClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hardamyggan.shitchatter;
+
+/**
+ *
+ * @author phobr
+ */
+public class SimpleClass {
+    
+    //TODO: Här ska du skapa din magi Victor :)
+    
+}
